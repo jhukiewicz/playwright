b92b1967a316c8190669b7191d44de0b4baa092e
Jakub Hukiewicz <j.hukiewicz12@gmail.com>
2025-03-12 17:20:57 +0100

    First

---
+++ playwright.config.js
@@ -0,0 +1,18 @@
+// @ts-check
+import {defineConfig, devices} from '@playwright/test';
+
+const config = ({
+    testDir: './tests',
+    timeout: 40 * 1000,
+    expect: {
+        timeout: 5000
+    },
+    reporter: 'html',
+    use: {
+        browserName: "chromium",
+        headless: false
+    }
+});
+
+module.exports = config
+

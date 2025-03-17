057dc683a37b659a8f3faaed1bfb85c67d07a615
Jakub Hukiewicz <j.hukiewicz12@gmail.com>
2025-03-17 14:00:35 +0100

    Smth

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -3,8 +3,29 @@ const {test, expect} = require('@playwright/test')
 test('Browser Context Playwright test', async ({browser}) => {
     const context = await browser.newContext();
     const page = await context.newPage();
+    const userName = page.locator('#username');
+    const passwordField = page.locator('#password');
+    const signInButton = page.locator('#signInBtn');
+    const wrongUsername = "rahulshetty";
+    const password = "learning";
+    const correctUsername = "rahulshettyacademy";
+    const cardTitles = page.locator(".card-body a");
+
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
     console.log(await page.title());
+
+    await userName.fill(wrongUsername);
+    await passwordField.fill(password);
+    await signInButton.click();
+
+    await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+
+    await userName.fill(correctUsername);
+    await passwordField.fill(password)
+    await signInButton.click();
+
+    await cardTitles.nth(0)
 
 })
 
